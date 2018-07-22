70ed4ac53f1c067077e91903504ec9da37b72cb6
LIULE8 <scau_lzc@163.com>
2018-07-22 12:54:07 +0800

    add two api (update and delete)

--- src/main/java/com/leo/controller/TodoListController.java
+++ src/main/java/com/leo/controller/TodoListController.java
@@ -4,10 +4,7 @@ import com.leo.dao.TodoRepository;
 import com.leo.model.Todo;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +29,31 @@ public class TodoListController {
         }
         repository.save(todo);
         return "add todo success";
+    }
+
+    @PutMapping("/todo/update/{id}")
+    public String updateTodo(@PathVariable("id") String id, Todo todo) {
+        if (!StringUtils.isNotBlank(id)) {
+            return "update todo fail";
+        }
+        repository.saveAndFlush(todo);
+        return "update todo success";
+    }
+
+
+    @DeleteMapping("/todo/delete/{id}")
+    public String deleteTodo(@PathVariable("id") String id) {
+        if (!StringUtils.isNotBlank(id)) {
+            return "update todo fail";
+        }
+        try {
+            Todo todo = repository.getOne(id);
+            repository.delete(todo);
+            return "delete success";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "can not find todo by this id";
     }
 
     private boolean checkTodo(Todo todo) {
