d20a5a21f15023a62b7a779f6ed6e2d2a3c63406
LIULE8 <scau_lzc@163.com>
2018-07-22 12:44:56 +0800

    create a api(add) to add a todo

--- src/main/java/com/leo/controller/TodoListController.java
+++ src/main/java/com/leo/controller/TodoListController.java
@@ -2,8 +2,11 @@ package com.leo.controller;
 
 import com.leo.dao.TodoRepository;
 import com.leo.model.Todo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +23,27 @@ public class TodoListController {
     @GetMapping("/todo/showAll")
     public List<Todo> showAll() {
         return repository.findAll();
+    }
+
+    @PostMapping("/todo/add")
+    public String addTodo(Todo todo) {
+        if (!checkTodo(todo)) {
+            return "add todo fail";
+        }
+        repository.save(todo);
+        return "add todo success";
+    }
+
+    private boolean checkTodo(Todo todo) {
+        boolean flag = true;
+        if (todo == null) {
+            flag = false;
+        } else if (!StringUtils.isNotBlank(todo.getId())) {
+            flag = false;
+        } else if (!StringUtils.isNotBlank(todo.getName())) {
+            flag = false;
+        }
+        return flag;
     }
 
 }
